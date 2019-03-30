c9be01e7a8f2ddd54ba3918da04a0d8710f6a213
Wade Oghenejabor <oghenejaborwade@gmail.com>
2019-03-30 11:44:28 +0100

    new project

---
+++ assets/js/todos.js
@@ -0,0 +1,37 @@
+;// Check off Specific Todos By Clicking
+$("ul").on("click", "li", function(){
+   $(this).toggleClass("completed");
+});
+
+
+
+// Click on X to delete todo
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+// Adding new Todos
+$("input[type='text']").keypress(function(event){
+    if (event.which === 13) {
+        // Grabbing new todo text from input
+            var todoText = $(this).val();
+            $(this).val("");
+            // Create a new li and add to ul
+            $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+    }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+});
+
+
+
+
+
+
+
+
